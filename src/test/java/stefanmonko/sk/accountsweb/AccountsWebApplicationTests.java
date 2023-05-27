18c9dd283b87f08716fb195e0fd74385b38c1cd3
Stefan Monko <monkos@posam.sk>
2023-05-27 21:42:07 +0200

    workflow and test update

--- src/test/java/stefanmonko/sk/accountsweb/AccountsWebApplicationTests.java
+++ src/test/java/stefanmonko/sk/accountsweb/AccountsWebApplicationTests.java
@@ -1,13 +1,13 @@
 package stefanmonko.sk.accountsweb;
 
-// import org.junit.jupiter.api.Test;
-// import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
-// class AccountsWebApplicationTests {
+@SpringBootTest
+class AccountsWebApplicationTests {
 
-// 	@Test
-// 	void contextLoads() {
-// 	}
+	@Test
+	void contextLoads() {
+	}
 
-// }
+}
